e1585404676b8f47e499fad65a91f81f00df5bed
Samad Rahman~ <samadsrahman@gmail.com>
2024-01-03 18:47:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
+import "video.js/dist/video-js.css";
+// import "./style.css";
+import { useParams } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const videoRef = useRef(null);
+  const [videoData, setVideoData] = useState(null)
+  const [videoPath, setVideoPath] = useState("")
+  const [trackPath, setTrackPath] = useState("")
+  let videoPlayer;
+  const {id} = useParams(); 
+  const [allAnswers, setAllAnswers] = useState([]);
+  const [isDisplay, setIsDisplay] = useState(true);
+  const [question, setQuestion] = useState("");
+  const [questionData, setQuestionData] = useState([]);
+  let [currentQuestion, setCurrentQuestion] = useState(0);
+  console.log(id)
+  useEffect(() => {
+    //fetching data for questions
+    fetch(`https://videojs.onrender.com/api/v1/getVideoById/${id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.data);
+        setVideoData(data.data)
+      })
+      .catch((error) => {
+        console.error("There was a problem fetching the data:", error);
+      });
+  }, []);
+useEffect(()=>{
+  if(videoData){
+    console.log(videoData.video_path)
+    setVideoPath(videoData.video_path)
+    setTrackPath(videoData.vtt_path)
+  }
+},[videoData])
+  useEffect(() => {
+    if (videoRef.current) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      videoPlayer = videoRef.current;
+      let tracks = videoPlayer.textTracks;
+      let questionTrack;
+
+      for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+
+        if (track.label === "questions") {
+          track.mode = "hidden";
+          // Store it for usage outside of the loop.
+          questionTrack = track;
+        }
+      }
+      // questionTrack.onCueChange(()=>console.log("cue"))
+      questionTrack.addEventListener("cuechange", () => {
+        displayMCQOverlay(questionData[currentQuestion]);
+      });
+    }
+    //
+  }, [questionData, currentQuestion]);
+  const displayMCQOverlay = (data) => {
+    if (questionData.length > 0) {
+      setQuestion(data.question.text);
+      videoPlayer.pause();
+      console.log(videoPlayer);
+      setIsDisplay(false);
+      setAllAnswers([...data.incorrectAnswers, data.correctAnswer]);
+      shuffleArray(allAnswers);
+    }
+  };
+
+  function handleDone() {
+    console.log(videoPlayer);
+    setIsDisplay(true); 
+    setCurrentQuestion(currentQuestion + 1);
+    setIsDisplay(true);
+    handlePlay();
+  }
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  const handlePlay = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={isDisplay ? {} : { display: "none" }}>
+        <div data-vjs-player>
+          <video
+            ref={videoRef}
+            className="video-js vjs-big-play-centered"
+            controls
+            width="430"
+            height="200"
+          >
+            <source src={videoPath} type="video/mp4" />
+            <track
+              src={trackPath}
+              label="questions"
+              kind="metadata"
+              default={true}
+            />
+          </video>
+        </div>
+      </div>
+      <div className="mcqSection" style={!isDisplay ? {} : { display: "none" }}>
+        <h3>{question}</h3>
+        {allAnswers.map((answer) => (
+          <div key={answer}>
+            <input type="radio" name="answer" value={answer} />
+            <label>{answer}</label>
+            <br/>
+          </div>
+        ))}
+        <button className="doneBtn" onClick={handleDone}>
+          Done
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+// 
